e768b2f7cb07080dba97fe18b9b9b70360a442a4
Adam Cooper <adambco@me.com>
2014-10-31 18:24:04 -0500

    Created Project Files

---
+++ PunctualTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PunctualTime
+//
+//  Created by Adam Cooper on 10/31/14.
+//  Copyright (c) 2014 The Timers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PunctualTime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PunctualTime
+//
+//  Created by Adam Cooper on 10/31/14.
+//  Copyright (c) 2014 The Timers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
