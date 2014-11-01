c8a342967ff14199c1c406d99d8a16018ce72f6c
Adam Cooper <adambco@me.com>
2014-10-31 19:42:20 -0500

    Create UserLocationController

---
+++ UserLocationController.h
@@ -0,0 +1,13 @@
+//
+//  UserLocationController.h
+//  PunctualTime
+//
+//  Created by Adam Cooper on 10/31/14.
+//  Copyright (c) 2014 The Timers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserLocationController : NSObject
+
+@end
---
+++ UserLocationController.m
@@ -0,0 +1,13 @@
+//
+//  UserLocationController.m
+//  PunctualTime
+//
+//  Created by Adam Cooper on 10/31/14.
+//  Copyright (c) 2014 The Timers. All rights reserved.
+//
+
+#import "UserLocationController.h"
+
+@implementation UserLocationController
+
+@end
