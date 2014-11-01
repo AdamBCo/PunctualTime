f2612049bd3afb629cacc9d843d3c38762eeba30
Nathan Hosselton <nathanhosselton@gmail.com>
2014-10-31 19:39:48 -0500

    refactor of ViewController to RootViewController

---
+++ PunctualTime/RootViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  PunctualTime
+//
+//  Created by Adam Cooper on 10/31/14.
+//  Copyright (c) 2014 The Timers. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    //
+}
+
+@end
