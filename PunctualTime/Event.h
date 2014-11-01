7a941b738db7c56531852ecc3456e519f376e51b
Nathan Hosselton <nathanhosselton@gmail.com>
2014-10-31 20:41:09 -0500

    created and setup Event class

---
+++ PunctualTime/Event.h
@@ -0,0 +1,24 @@
+//
+//  Event.h
+//  PunctualTime
+//
+//  Created by Nathan Hosselton on 10/31/14.
+//  Copyright (c) 2014 The Timers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+
+@interface Event : MKMapItem
+
+@property (readonly) NSString* eventName;
+@property (readonly) NSString* startingAddress;
+@property (readonly) NSString* endingAddress;
+@property (readonly) NSDate* desiredArrivalTime;
+
+- (instancetype)initWithEventName:(NSString *)name
+                  startingAddress:(NSString *)startingAddress
+                    endingAddress:(NSString *)endingAddress
+                      arrivalTime:(NSDate *)arrivalTime;
+
+@end
