1ac2f189b0aef445f38596452cd114c54dac0b74
Nathan Hosselton <nathanhosselton@gmail.com>
2014-11-01 12:24:16 -0500

    Created EventController class and organized class files into groups

---
+++ PunctualTime/EventController.h
@@ -0,0 +1,13 @@
+//
+//  EventController.h
+//  PunctualTime
+//
+//  Created by Nathan Hosselton on 11/1/14.
+//  Copyright (c) 2014 The Timers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EventController : NSObject
+
+@end
---
+++ PunctualTime/EventController.m
@@ -0,0 +1,13 @@
+//
+//  EventController.m
+//  PunctualTime
+//
+//  Created by Nathan Hosselton on 11/1/14.
+//  Copyright (c) 2014 The Timers. All rights reserved.
+//
+
+#import "EventController.h"
+
+@implementation EventController
+
+@end
