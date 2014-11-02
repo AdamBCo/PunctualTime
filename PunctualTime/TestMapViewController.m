905ec6ee2a059e35f829432a69f8ce885e6f8f29
Adam Cooper <adambco@me.com>
2014-11-02 01:49:26 -0500

    Calculated the time it would take between two points.

--- PunctualTime/TestMapViewController.m
+++ PunctualTime/TestMapViewController.m
@@ -15,9 +15,11 @@
 @property (weak, nonatomic) IBOutlet UISearchBar *startSearchBar;
 @property (weak, nonatomic) IBOutlet UISearchBar *destinationSearchBar;
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+@property (weak, nonatomic) IBOutlet UITextView *directionsTextView;
 @property MKPointAnnotation *userDestination;
-@property CLLocation *userLocation;
-@property NSArray *searchLocations;
+@property NSArray *sourceLocations;
+@property NSArray *destinationLocations;
 
 @property LocationSearchController *locationSearchController;
 
@@ -28,8 +30,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.startSearchBar.delegate = self;
-        self.destinationSearchBar.delegate = self;
+    self.destinationSearchBar.delegate = self;
     self.locationSearchController = [LocationSearchController new];
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -40,23 +43,80 @@
 #pragma mark - SearchBar
 
 -(void)searchBarSearchButtonClicked:(UISearchBar *)searchBar{
-    NSLog(@"text: %@",self.startSearchBar.text);
-    [self.locationSearchController searchLocations:self.startSearchBar.text withCompletion:^(NSArray *placemarks) {
-        self.searchLocations = [NSArray arrayWithArray:placemarks];
-        NSLog(@"%@",self.searchLocations);
-    }];
+
+    if(searchBar == self.startSearchBar){
+        [self.locationSearchController searchLocations:self.startSearchBar.text withCompletion:^(NSArray *placemarks) {
+            self.sourceLocations = [NSArray arrayWithArray:placemarks];
+            [self.mapView addAnnotations:self.sourceLocations];
+            [self.mapView showAnnotations:self.sourceLocations animated:YES];
+            self.userDestination = self.sourceLocations.firstObject;
+            [self.startSearchBar resignFirstResponder];
+            [self getDirections];
+            NSLog(@"%@",self.sourceLocations);
+        }];
+    }
+    else if(searchBar == self.destinationSearchBar)
+    {
+        [self.locationSearchController searchLocations:self.destinationSearchBar.text withCompletion:^(NSArray *placemarks) {
+            self.destinationLocations = [NSArray arrayWithArray:placemarks];
+            [self.mapView addAnnotations:self.destinationLocations];
+            [self.destinationSearchBar resignFirstResponder];
+            [self.mapView showAnnotations:self.destinationLocations animated:YES];
+            [self getDirections];
+            NSLog(@"%@",self.sourceLocations);
+        }];
+    }
     NSLog(@"Hello");
 }
 
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(void)getDirections{
+    MKDirectionsRequest *request = [[MKDirectionsRequest alloc] init];
+    [request setSource:[MKMapItem mapItemForCurrentLocation]];
+    MKPlacemark *mkDest = [[MKPlacemark alloc] initWithCoordinate:self.userDestination.coordinate addressDictionary:nil];
+    [request setDestination:[[MKMapItem alloc] initWithPlacemark:mkDest]];
+    [request setTransportType:MKDirectionsTransportTypeWalking];
+    [request setRequestsAlternateRoutes:NO];
+    MKDirections *directions = [[MKDirections alloc] initWithRequest:request];
+    [directions calculateDirectionsWithCompletionHandler:^(MKDirectionsResponse *response, NSError *error) {
+        if ( !error && [response routes] > 0) {
+            MKRoute *route = [[response routes] objectAtIndex:0];
+            NSString *text = [NSString stringWithFormat:@"It will take: %f hours",route.expectedTravelTime / 3600];
+            self.directionsTextView.text = text;
+        }
+    }];
 }
-*/
+
+-(void)getDirectionsAuto{
+    MKDirectionsRequest *request = [[MKDirectionsRequest alloc] init];
+    [request setSource:[MKMapItem mapItemForCurrentLocation]];
+    MKPlacemark *mkDest = [[MKPlacemark alloc] initWithCoordinate:self.userDestination.coordinate addressDictionary:nil];
+    [request setDestination:[[MKMapItem alloc] initWithPlacemark:mkDest]];
+    [request setTransportType:MKDirectionsTransportTypeAutomobile];
+    [request setRequestsAlternateRoutes:NO];
+    MKDirections *directions = [[MKDirections alloc] initWithRequest:request];
+    [directions calculateDirectionsWithCompletionHandler:^(MKDirectionsResponse *response, NSError *error) {
+        if ( !error && [response routes] > 0) {
+            MKRoute *route = [[response routes] objectAtIndex:0];
+            NSLog(@"The route: %f",route.distance);
+            NSString *text = [NSString stringWithFormat:@"It will take: %f hours",route.expectedTravelTime / 3600];
+            self.directionsTextView.text = text;
+        }
+    }];
+}
+
+- (IBAction)segmentedControl:(id)sender {
+
+    switch (self.segmentedControl.selectedSegmentIndex) {
+        case 0:
+            [self getDirections];
+            break;
+        case 1:
+            [self getDirectionsAuto];
+            break;
+
+        default:
+            break;
+    }
+}
 
 @end
