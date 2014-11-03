d50c1408be35fc17c446420eed0ed431e1954694
Adam Cooper <adambco@me.com>
2014-11-03 16:59:29 -0600

    Finished Google Places Autocomplete

--- PunctualTime/SearchTableViewController.m
+++ PunctualTime/SearchTableViewController.m
@@ -8,7 +8,12 @@
 
 #import "SearchTableViewController.h"
 
+NSString *const apiURI = @"https://maps.googleapis.com/maps/api/place/autocomplete/output?parameters";
+NSString *const apiKey = @"AIzaSyBB2Uc2kK0P3zDKwgyYlyC8ivdDCSyy4xg";
+
 @interface SearchTableViewController () <UISearchBarDelegate>
+
+
 @property (weak, nonatomic) IBOutlet UISearchBar *searchTextField;
 @property NSMutableArray *localSearchQueries;
 @property NSMutableArray *pastSearchQueries;
@@ -22,6 +27,7 @@
     self.pastSearchQueries = [NSMutableArray array];
     self.localSearchQueries = [NSMutableArray array];
     self.searchTextField.delegate = self;
+
 
 }
 
@@ -37,6 +43,11 @@
 - (void)searchAutocompleteLocationsWithSubstring:(NSString *)substring{
     [self.pastSearchQueries removeAllObjects];
     NSLog(@"Sub: %@", substring);
+
+    [self retrieveGooglePlaceInformation:substring withCompletion:^(NSArray * results) {
+        [self.pastSearchQueries addObjectsFromArray:results];
+        [self.tableView reloadData];
+    }];
     NSLog(@"Search numbers: %lu", (unsigned long)self.pastSearchQueries.count);
 
     for(NSString *pastSearch in self.localSearchQueries) {
@@ -46,16 +57,40 @@
 
         }
     }
-    [self.tableView reloadData];
 }
 
 #pragma mark UITextFieldDelegate methods
 
 -(BOOL)searchBar:(UISearchBar *)searchBar shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text{
     NSString *substring = [NSString stringWithString:self.searchTextField.text];
+    substring= [substring stringByReplacingOccurrencesOfString:@" " withString:@"+"];
     substring = [substring stringByReplacingCharactersInRange:range withString:text];
     [self searchAutocompleteLocationsWithSubstring:substring];
     return YES;
+}
+
+
+
+#pragma mark - Google API Requests
+
+-(void)retrieveGooglePlaceInformation:(NSString*)searchWord withCompletion:(void (^)(NSArray *))complete{
+
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"https://maps.googleapis.com/maps/api/place/autocomplete/json?input=%@&types=geocode&language=en&key=%@",searchWord,apiKey]];
+
+    NSURLSessionConfiguration *defaultConfigObject = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *delegateFreeSession = [NSURLSession sessionWithConfiguration: defaultConfigObject delegate: nil delegateQueue: [NSOperationQueue mainQueue]];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+
+    NSURLSessionDataTask *task = [delegateFreeSession dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        NSDictionary *jSONresult = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:nil];
+        NSArray *results = [jSONresult valueForKey:@"predictions"];
+
+        NSLog(@"We got %lu locations from google.",(unsigned long)results.count);
+        complete(results);
+
+    }];
+    [task resume];
+
 }
 
 
@@ -67,11 +102,28 @@
 
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSDictionary *searchResult = [self.pastSearchQueries objectAtIndex:indexPath.row];
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SearchCell" forIndexPath:indexPath];
-    cell.textLabel.text = [self.pastSearchQueries objectAtIndex:indexPath.row];
+    cell.textLabel.text = [searchResult objectForKey:@"description"];
     
     return cell;
 }
+
+
+- (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section{
+
+    UIView *footerView  = [[UIView alloc] initWithFrame:CGRectMake(0, 500, 320, 70)];
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"powered-by-google-on-white"]];
+    imageView.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin;
+    imageView.frame = CGRectMake(110,10,100,12);
+    [footerView addSubview:imageView];
+
+    self.tableView.tableFooterView = footerView;
+
+    return footerView;
+
+}
+
 
 
 @end
