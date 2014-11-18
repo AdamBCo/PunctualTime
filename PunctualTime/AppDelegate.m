395e57bdf28a993fba7a251ac285c5ada6688aea
Adam Cooper <adambco@me.com>
2014-11-17 20:06:10 -0600

    Fixed Issues

--- PunctualTime/AppDelegate.m
+++ PunctualTime/AppDelegate.m
@@ -47,6 +47,17 @@ static NSString* FINAL_BUTTON = @"I'm leaving!";
         [application registerUserNotificationSettings:settings];
     }
 
+
+
+    [[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
+    [[UINavigationBar appearance] setTitleTextAttributes: [NSDictionary dictionaryWithObjectsAndKeys:
+                                                           [UIColor colorWithRed:245.0/255.0 green:245.0/255.0 blue:245.0/255.0 alpha:1.0], NSForegroundColorAttributeName,
+                                                           [UIFont fontWithName:@"HelveticaNeue-Thin" size:20.0], NSFontAttributeName, nil]];
+
+    [[UIBarButtonItem appearance] setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:
+                                                          [UIFont fontWithName:@"HelveticaNeue-Thin" size:16.0],
+                                                          NSFontAttributeName, nil] forState:UIControlStateNormal];
+
     return YES;
 }
 
