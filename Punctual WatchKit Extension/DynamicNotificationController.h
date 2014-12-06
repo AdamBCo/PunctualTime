bcff77ba32f86e4901e278d1b805afe190fb3cff
Nathan Hosselton <nathanhosselton@gmail.com>
2014-12-06 15:25:26 -0600

    add DynamicNotificationController class to project

---
+++ Punctual WatchKit Extension/DynamicNotificationController.h
@@ -0,0 +1,13 @@
+//
+//  DynamicNotificationController.h
+//  PunctualTime
+//
+//  Created by Nathan Hosselton on 12/6/14.
+//  Copyright (c) 2014 The Timers. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+@interface DynamicNotificationController : WKUserNotificationInterfaceController
+
+@end
