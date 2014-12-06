b7856570139c093eef32c0f0f059a5ec1511eaa8
Nathan Hosselton <nathanhosselton@gmail.com>
2014-12-06 16:53:50 -0600

    Next event loaded in InterfaceController

--- Punctual WatchKit Extension/InterfaceController.m
+++ Punctual WatchKit Extension/InterfaceController.m
@@ -7,37 +7,60 @@
 //
 
 #import "InterfaceController.h"
+#import "Event.h"
 
+static NSString* const appGroupIdentifier = @"group.com.Punctual.app";
 
 @interface InterfaceController()
 
 @property (strong, nonatomic) IBOutlet WKInterfaceLabel *eventLabel;
 @property (strong, nonatomic) IBOutlet WKInterfaceTimer *eventTimer;
+@property Event* nextEvent;
 
 @end
 
 
 @implementation InterfaceController
 
+#pragma mark - Lifecycle
+
 - (instancetype)initWithContext:(id)context {
     self = [super initWithContext:context];
     if (self){
         // Initialize variables here.
         // Configure interface objects here.
-        NSLog(@"%@ initWithContext", self);
-        
+
+        [self loadEvent]; // Get the next upcoming event
     }
     return self;
 }
 
 - (void)willActivate {
     // This method is called when watch view controller is about to be visible to user
-    NSLog(@"%@ will activate", self);
 }
 
 - (void)didDeactivate {
     // This method is called when watch view controller is no longer visible
-    NSLog(@"%@ did deactivate", self);
+}
+
+
+#pragma mark - Data persistence
+
+- (NSURL *)documentsDirectory
+{
+    // TODO: Migrate user data to new container
+    NSFileManager* fileManager = [NSFileManager defaultManager];
+    NSURL* fileURL = [fileManager containerURLForSecurityApplicationGroupIdentifier:appGroupIdentifier];
+
+    return fileURL;
+}
+
+- (void)loadEvent // This method should NEVER be public
+{
+    NSURL* plist = [[self documentsDirectory] URLByAppendingPathComponent:@"events.plist"];
+    NSArray* savedData = [NSArray arrayWithContentsOfURL:plist];
+
+    self.nextEvent = [NSKeyedUnarchiver unarchiveObjectWithData:[savedData firstObject]];
 }
 
 @end
