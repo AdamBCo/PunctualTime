6220006ce324d809002e65f9e7fb1d70b534f5e4
Nathan Hosselton <nathanhosselton@gmail.com>
2014-12-07 11:08:13 -0600

    View all events in app

--- Punctual WatchKit Extension/InterfaceController.m
+++ Punctual WatchKit Extension/InterfaceController.m
@@ -17,7 +17,6 @@ static NSString* const appGroupIdentifier = @"group.com.Punctual.app";
 
 @property (strong, nonatomic) IBOutlet WKInterfaceLabel *eventLabel;
 @property (strong, nonatomic) IBOutlet WKInterfaceTimer *eventTimer;
-@property EventLite* nextEvent;
 @property NSMutableArray *eventStore;
 @property int count;
 
@@ -36,7 +35,6 @@ static NSString* const appGroupIdentifier = @"group.com.Punctual.app";
 
         [self loadEvents]; // Get the next upcoming
         self.count = 0;
-
     }
     return self;
 }
@@ -55,34 +53,36 @@ static NSString* const appGroupIdentifier = @"group.com.Punctual.app";
 
 - (IBAction)onPreviousButtonTapped
 {
-    self.count--;
-    [self.eventLabel setText:((EventLite *)[self.eventStore objectAtIndex:self.count]).eventName];
-    [self.eventTimer setDate:((EventLite *)[self.eventStore objectAtIndex:self.count]).lastLeaveTime];
-
-    if(self.count < self.eventStore.count-1)
+    if (self.count > 0)
     {
+        self.count--;
+        [self.eventLabel setText:((EventLite *)[self.eventStore objectAtIndex:self.count]).eventName];
+        [self.eventTimer setDate:((EventLite *)[self.eventStore objectAtIndex:self.count]).lastLeaveTime];
+    }
+
+    if(self.count < self.eventStore.count-1) {
         [self.nextButton setEnabled:YES];
     }
 
-    if(self.count == 0){
+    if(self.count == 0) {
         [self.previousButton setEnabled:NO];
     }
-
 }
 
 -(IBAction)onNextButtonTapped
 {
-    self.count++;
-    if (!(self.count == self.eventStore.count-1)) {
-    [self.eventLabel setText:((EventLite *)[self.eventStore objectAtIndex:self.count]).eventName];
-    [self.eventTimer setDate:((EventLite *)[self.eventStore objectAtIndex:self.count]).lastLeaveTime];
+    if (self.count < self.eventStore.count-1)
+    {
+        self.count++;
+        [self.eventLabel setText:((EventLite *)[self.eventStore objectAtIndex:self.count]).eventName];
+        [self.eventTimer setDate:((EventLite *)[self.eventStore objectAtIndex:self.count]).lastLeaveTime];
     }
 
     if (self.count == self.eventStore.count-1) {
         [self.nextButton setEnabled:NO];
     }
 
-    if (self.count > 0){
+    if (self.count > 0) {
         [self.previousButton setEnabled:YES];
     }
 }
@@ -103,15 +103,13 @@ static NSString* const appGroupIdentifier = @"group.com.Punctual.app";
 {
     NSURL* plist = [[self documentsDirectory] URLByAppendingPathComponent:@"events.plist"];
     NSArray* savedData = [NSArray arrayWithContentsOfURL:plist];
-
     self.eventStore = [NSMutableArray new];
+
     [NSKeyedUnarchiver setClass:[EventLite class] forClassName:@"Event"];
-    self.nextEvent = [NSKeyedUnarchiver unarchiveObjectWithData:[savedData firstObject]];
     for (NSData *data in savedData){
         EventLite *event = [NSKeyedUnarchiver unarchiveObjectWithData:data];
         [self.eventStore addObject:event];
     }
-
 }
 
 @end
